27ed82a0089f86246d5febd0ec7a7d49e8464873
Leslie Counterman <LCounterman1@gmail.com>
2021-09-28 21:51:06 -0400

    first commit

---
+++ ClassLibrary09_28/Facebook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary09_28
+{
+    public class Facebook
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int AgeInYears { get; set; }
+        public string Gender { get; set; }
+        public string Location { get; set; }
+
+        public Facebook(string firstName, string lastName, int age, string gender, string location)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            AgeInYears = age;
+            Gender = gender;
+            Location = location;
+
+        }
+ 
+    }
+
+ 
+}
---
+++ CondingChallenge09_28/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary09_28;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondingChallenge09_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facebook newProfile = new Facebook("Leslie", "Counterman", 30, "Female", "Indiana");
+
+
+
+            Console.WriteLine(newProfile.FirstName);
+            Console.WriteLine(newProfile.LastName);
+            Console.WriteLine(newProfile.AgeInYears);
+            Console.WriteLine(newProfile.Gender);
+            Console.WriteLine(newProfile.Location);
+
+            Console.ReadLine();
+        }
+    }
+}
